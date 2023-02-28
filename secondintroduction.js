99bf3b7fc9c2d3da6c5bac95c536d9e9c826570b
Rinier Dapugo <rinierdapugo@gmail.com>
2023-02-28 18:04:11 +0800

    Add Object Introduction to this folder

---
+++ secondintroduction.js
@@ -0,0 +1,13 @@
+const heroCharacter = {
+    name: "Rinier",
+    status: "Marksman",
+    damage: 500,
+    isStrong: true,
+}
+
+console.log(heroCharacter);
+console.log(heroCharacter.name);
+heroCharacter.damage = 1000
+console.log(heroCharacter);
+heroCharacter.speed = 10;
+console.log(heroCharacter);
